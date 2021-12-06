cf6c2cfe84c2dc8942bfef9972d26246074666b2
peteryoungy <49567972+peteryoungy@users.noreply.github.com>
2021-12-06 05:43:28 -0500

    fix table name case bug

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,12 +7,21 @@ CREATE TABLE IF NOT EXISTS USERS (
     lastname VARCHAR(256)
 );
 
-CREATE TABLE IF NOT EXISTS FILES (
+CREATE TABLE IF NOT EXISTS users (
+     userid INT PRIMARY KEY auto_increment,
+     username VARCHAR(256),
+     salt VARCHAR(256),
+     password VARCHAR(256),
+     firstname VARCHAR(256),
+     lastname VARCHAR(256)
+);
+
+CREATE TABLE IF NOT EXISTS files (
      fileId INT PRIMARY KEY auto_increment,
      filename VARCHAR(256),
      contenttype VARCHAR(256),
      filesize VARCHAR(256),
      userid INT,
      filedata BLOB,
-     foreign key (userid) references USERS(userid)
+     foreign key (userid) references users(userid)
 );
