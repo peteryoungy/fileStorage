3a0ab7ff71fc020838359ca05f7c222a7fd6c620
peteryoungy <49567972+peteryoungy@users.noreply.github.com>
2021-12-06 03:26:28 -0500

    use mysql as permanent db

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS USERS (
     userid INT PRIMARY KEY auto_increment,
-    username VARCHAR(20),
-    salt VARCHAR,
-    password VARCHAR,
-    firstname VARCHAR(20),
-    lastname VARCHAR(20)
+    username VARCHAR(256),
+    salt VARCHAR(256),
+    password VARCHAR(256),
+    firstname VARCHAR(256),
+    lastname VARCHAR(256)
 );
 
 CREATE TABLE IF NOT EXISTS FILES (
      fileId INT PRIMARY KEY auto_increment,
-     filename VARCHAR,
-     contenttype VARCHAR,
-     filesize VARCHAR,
+     filename VARCHAR(256),
+     contenttype VARCHAR(256),
+     filesize VARCHAR(256),
      userid INT,
      filedata BLOB,
      foreign key (userid) references USERS(userid)
